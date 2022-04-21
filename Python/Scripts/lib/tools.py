ba629df5bece38fde4bae9be563553fd63bac6c5
Colin Vo <ckv6442@rit.edu>
2022-04-20 20:47:55 -0400

    Updated tools.py
    Added test algorithms. Comments that say "this works" are algorithms with most progress.

--- Python/Scripts/lib/tools.py
+++ Python/Scripts/lib/tools.py
@@ -1,5 +1,6 @@
 import time
 import os.path
+<<<<<<< Updated upstream
 from tkinter.filedialog import askdirectory
 
 def get_directory():
@@ -22,6 +23,9 @@ def is_number(num):
         flag = False
     
     return flag
+=======
+import math
+>>>>>>> Stashed changes
 
 def convert_ticks_to_rpm(ticks, tpr):
     xData = [0]
@@ -128,13 +132,13 @@ def funny2(xdata, ydata):
     for i in range(0, len(buffer)):
         # outlier filter, change inequality
         # lower value = more filtered
-        if abs(buffer[i]) > 5:
+        if abs(buffer[i]) > 30:
             indexes.append(i)
     newXData = []
     newYData = []
     newBuffer = []
     for i in range(len(buffer)):
-        if i not in indexes:
+        if i not in indexes and (i-1) not in indexes and (i+1) not in indexes:
             newXData.append(xdata[i])
             newYData.append(ydata[i])
             newBuffer.append(buffer[i])
@@ -255,3 +259,109 @@ def write(words):
     f = open(current_file, "a")
     f.write(words + "\n")
     f.close()
+
+# This works
+def angle_avg(a, b):
+  a -= 128
+  b -= 128
+  sum_angle = ((b - a + 128) % 256 - 128) / 2
+  sum_angle = (a + sum_angle + 128) % 256 - 128
+  return (sum_angle + 128) 
+
+# This works
+def angle_avg_set(items):
+    if len(items) > 0:
+        if not (math.log2(len(items)) % 1.0 == 0):
+            return -1.0
+    else: return -1.0
+    arr = []
+    tempList = items.copy()
+    while len(tempList) > 1:
+        arr.append(angle_avg(tempList[0], tempList[1]))
+        tempList.pop(0)
+        tempList.pop(0)
+    if len(arr) == 1:
+        return arr[0]   
+    return angle_avg_set(arr)
+
+# This works
+def rolling_angle_filter(data, depth_exp, c=64):
+    depth = 2**depth_exp
+    if len(data) < depth:
+        depth = len(data)
+    buffer = data[0:depth]
+    #print(len(data))
+    for i in range(len(data)):
+        #print(i, ",", (i % depth), ",", len(data), ",", len(buffer))
+        buffer[i % depth] = data[i]
+        if i < c:
+            data[i] = angle_avg_set(buffer[i:i+c]) # this comes from prev message
+        else:
+            data[i] = angle_avg_set(buffer[i-c:i+c]) # this comes from prev message
+        print(data[i])
+    return data
+
+# This probably has a very long computation time
+# See the normal tools.apply_rolling_filter for reference
+def rolling_angle_filter2(data, depth_exp):
+  depth = 2**depth_exp # only allow the depth to be a power of 2
+  if len(data) < depth:
+    depth = len(data)
+  buffer = data[0:depth]
+  for i in range(len(data)):
+    buffer[i % depth] = data[i]
+    # the following line should be the only different one
+    # its just getting the average using our own function instead
+    data[i] = angle_avg_set(buffer.copy()) # this comes from prev message
+  return data
+
+def rolling_angle_filter3(xData, data, depth_exp):
+    depth = 2**depth_exp
+    if len(data) < depth:
+        return data
+    newData = []
+    for i in range(depth, len(data) - 1):
+        buffer = data[i:depth].copy()
+        newData.append(angle_avg_set(buffer))
+    while len(xData) > len(data):
+        xData.pop(0)
+    return (xData, data)
+
+def angle_avg_A(a, b): # somewhat copied from stack overflow...
+  a -= 128
+  b -= 128
+  sum_angle = ((b - a + 128) % 256 - 128) / 2
+  sum_angle = (a + sum_angle + 128) % 256 - 128
+  return (sum_angle + 128) 
+
+def angle_avg_set_A(items):
+  if not (math.log2(len(items)) % 1.0 == 0.0): # the length of the array needs to be a power of 2 because of the average weighting problem
+    return -1.0
+  arr = []
+  while len(items) > 0:
+    arr.append(angle_avg(items[0], items[1])) # condense items into a new array. Average every two values
+    items.pop(0) # remove those two values
+    items.pop(0)
+  if len(arr) == 1:
+    return arr[0] # if you're fully condensed, give the final result
+  return angle_avg_set(arr) # Otherwise, try again with the condensed dataset
+
+def rolling_angle_filter_A(xData, data, depth_exp):
+    depth = 2**depth_exp
+    if len(data) < depth: # don't bother with short data
+        return data
+    newData = [] # new array for averaged data
+    for i in range(depth, len(data) - 1):
+        buffer = data[i-depth:i].copy() # get depth-length slice of data
+        newData.append(angle_avg_set_A(buffer))
+    while len(xData) > len(data):
+        xData.pop(0) # make them the same length (x and y)
+    return (xData, data)
+
+# hugh's function
+# post rollover function
+def find_max_diff(ydata):
+    diff_list = []
+    for i in range(0, len(ydata)-1):
+        diff_list.append(abs(ydata[i+1]-ydata[i]))
+    return max(diff_list)
