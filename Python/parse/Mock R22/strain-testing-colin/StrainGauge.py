db846e58801b64141b266e9ad4ca9d87337d09be
Colin Vo <ckv6442@rit.edu>
2022-04-21 21:07:25 -0400

    Fixed out of order data

--- Python/parse/Mock R22/strain-testing-colin/StrainGauge.py
+++ Python/parse/Mock R22/strain-testing-colin/StrainGauge.py
@@ -14,6 +14,7 @@ NUMDATAPOINTS = -1
 # will change to be optional later. leave at 0 and 999
 MINTIME = 0
 MAXTIME = 999
+USE_TIMING = True
 
 # have to use r"filepath" to get raw filepath & then replace \ with /
 # or just highlight one \ and press ctrl-d a few times and type a single /
@@ -26,6 +27,9 @@ filename.replace("\\", "/")
 constant = 75
 
 charsToReplace = ["[", "]", ","]
+replaceChar = ""
+
+lines = []
 
 print("Starting...")
 with open(filename) as x:
@@ -35,32 +39,32 @@ xdata = []
 ydata = []
 
 isxdata = True
-datapoints = 0
 
 # has stuff to fix & make more efficient
 # but is good enough for now
 print("Parsing File...")
 for i in lines:
-
-    linesplit = i.split()
-
-    for j in linesplit:
-        for character in charsToReplace:
-            j = j.replace(character, "")
-
+    line = i
+    for char in charsToReplace:
+        line = line.replace(char, replaceChar)
+    line = line.replace("  ", " ").split()
+    isxdata = True
+    for val in line:
         if isxdata:
-            if ((float(j) <= MINTIME) | (float(j) >= MAXTIME)): continue
-            xdata.append(float(j))
-            isxdata = False
+            xdata.append(float(val))
         else:
-            ydata.append(float(j))
-            isxdata = True
-            if datapoints == NUMDATAPOINTS: break
-        
-        if len(xdata) == len(ydata): datapoints += 1
-        
+            ydata.append(int(val))
+        isxdata = not isxdata
 
-    if datapoints == NUMDATAPOINTS: break
+if USE_TIMING:
+    tempXData = xdata.copy()
+    tempYData = ydata.copy()
+    xdata = []
+    ydata = []
+    for i in range(len(tempXData)):
+        if tempXData[i] >= MINTIME and tempXData[i] <= MAXTIME:
+            xdata.append(tempXData[i])
+            ydata.append(tempYData[i])
 
 # choose what algorithm is wanted here
 print("Calculating...")
