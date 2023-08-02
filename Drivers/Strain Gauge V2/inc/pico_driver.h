e87729ce06ca75e17692ccff9749ed4c740f6e68
HugGa <galloh@bxscience.edu>
2023-08-01 21:17:51 -0400

    Add v2 strain gauge board

---
+++ Drivers/Strain Gauge V2/inc/pico_driver.h
@@ -0,0 +1,22 @@
+#pragma once
+#define BUSY 24
+#define CNV 23
+#define I2CSTRTADDR 8
+#define ADC_BOARD     (0x08)
+#define STRAIN_BOARD  (0x10)
+#define IMU_BOARD     (0x18)
+
+#define OTHER_1       (0x20)
+#define OTHER_2       (0x28)
+#define OTHER_3       (0x30)
+#define OTHER_4       (0x38)
+#define OTHER_5       (0x40)
+#define OTHER_6       (0x48)
+#define OTHER_7       (0x50)
+#define OTHER_8       (0x58)
+#define OTHER_9       (0x60)
+#define OTHER_10      (0x68)
+#define OTHER_11      (0x70)
+
+#define BAUD_RATE     (9600)
+#define I2C_RATE      (1000000)
---
+++ Drivers/Strain Gauge V2/src/pico_driver.cpp
@@ -0,0 +1,65 @@
+#include "pico.h"
+#include <stdio.h>
+#include "hardware/spi.h"
+#include "hardware/irq.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "hardware/regs/clocks.h"
+#include "hardware/platform_defs.h"
+#include "hardware/clocks.h"
+#include "hardware/watchdog.h"
+#include "hardware/pll.h"
+#include "hardware/irq.h"
+#include "hardware/gpio.h"
+#include "pico_driver.h"
+#include <stdlib.h>
+#include <algorithm>
+#include <iterator>
+uint8_t get_i2c_addr()
+{
+    // Same stupid al-gore-rythm as the ADC board
+    int id = (gpio_get(8) << 2) + gpio_get(9) + gpio_get(10) + gpio_get(11);
+
+    return STRAIN_BOARD | id;
+}
+int core1_main()
+{
+    gpio_set_input_enabled(16, true);
+    
+}
+int main()
+{
+    gpio_init_mask(1 << 8 | 1 << 9 | 1 << 10 | 1 << 11);
+    gpio_set_input_enabled(8, true);
+    gpio_set_input_enabled(9, true);
+    gpio_set_input_enabled(10, true);
+    gpio_set_input_enabled(11, true);
+    constexpr int MHz = 1'000'000;
+    // We want *maximally* 27Mhz, spi_init will always round down to be below this if the division from whatever value our clock
+    // frequency is isn't clean, or throw an error.
+    spi_init(spi0, 27 * MHz);
+    i2c_init(i2c0, I2C_RATE);
+    i2c_set_slave_mode(i2c0, true, get_i2c_addr());
+    uint8_t zeros[4] = {0};
+    uint8_t vals[24 * 4] = {0};
+    while (true)
+    {
+        uint8_t request = 0;
+        i2c_read_raw_blocking(i2c0, &request, 1);
+        enum request_types
+        {
+            RETURN_FORMAT = 1,
+            DEFAULT = 0,
+        };
+        switch (request)
+        {
+            case RETURN_FORMAT:
+            // I haven't decided how this protocol will work, for now default to the same as the ADC board
+            default: {
+                spi_read_blocking(spi0, 0x00, vals, 24 * 4);
+                i2c_write_raw_blocking(i2c0, zeros, 4);
+            }
+            break;
+        }
+    }
+}
